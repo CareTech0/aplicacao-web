73ddb174831b9b97eee78e0b99aa91f99950882c
Henrique-Crispino <henrique.crispino@sptech.school>
2024-03-08 11:36:15 -0300

    Criação das seções 2 e 3, adição de suas respectivas imagens e iniciação da seção 4.

--- web-data-viz/site/public/js/indexScript.js
+++ web-data-viz/site/public/js/indexScript.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-    $('.carousel').slick({
-      autoplay: true,
-      autoplaySpeed: 5000,
-    });
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    pauseOnClick: false,
+    speed: 1000,
   });
-  
+});
+
+$('.carousel').on('click', function(event) {
+  event.stopPropagation();
+});
