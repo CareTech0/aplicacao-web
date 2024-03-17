6331916ec2c3c39303a9ab2da23d130231ac1ff3
Thawan Bruno Belo Rodrigues <thawan.rodriues@sptech.school>
2024-03-17 00:06:26 -0300

    Criando menu hamburguer para o index

--- web-data-viz/site/public/js/indexScript.js
+++ web-data-viz/site/public/js/indexScript.js
@@ -7,3 +7,15 @@ $(document).ready(function () {
     speed: 1000,
   });
 });
+
+function abrirMenu(){
+  const menu = document.getElementById("menu_cell")
+
+  menu.style.display = "flex"
+}
+
+function fecharMenu(){
+  const menu = document.getElementById("menu_cell")
+
+  menu.style.display = "none"
+}
