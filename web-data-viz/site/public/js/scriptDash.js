b5c4d545560905947767c70264cac5c979787726
amandageovanna <amanda.sousa@sptech.school>
2024-04-22 23:39:28 -0300

    chumbei os gráficos na dash filha, finalizei as paginas filhas, falta colocar as informações escritas.

---
+++ web-data-viz/site/public/js/scriptDash.js
@@ -0,0 +1,18 @@
+function direcionarRAM() {    
+      window.location.href = "dashboardFilhaRAM.html";
+} 
+function direcionarDISCO() {    
+    window.location.href = "dashboardFilhaDISCO.html";
+} 
+
+function direcionarREDE() {    
+    window.location.href = "dashboardFilhaREDE.html";
+} 
+
+function direcionarCPU() {    
+    window.location.href = "dashboardFilhaCPU.html";
+} 
+
+function voltar() {    
+    window.location.href = "dashboard.html";
+} 
