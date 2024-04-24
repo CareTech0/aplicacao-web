64b6f160946803ef9570ce8e1721c99050afdbd5
amandageovanna <amanda.sousa@sptech.school>
2024-04-24 01:09:22 -0300

    adicionei no menu a opcao de bloqueio de sites. adicionei modal de deslogar, criei a base da pagina de overview

--- web-data-viz/site/public/js/scriptDash.js
+++ web-data-viz/site/public/js/scriptDash.js
@@ -16,3 +16,29 @@ function direcionarCPU() {
 function voltar() {    
     window.location.href = "dashboard.html";
 } 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const deslogarModal = document.getElementById('deslogarModal');
+    const closeButtons = document.querySelectorAll('.close');
+    const spanDeslogar = document.querySelector('.item-menu-deslogar');
+    
+    function abrirDeslogarModal() { 
+        deslogarModal.style.display = 'block';
+    }
+    
+    spanDeslogar.addEventListener('click', function() {
+        abrirDeslogarModal(); 
+    });
+    
+    closeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            deslogarModal.style.display = 'none';        
+        });
+    });
+    
+    window.addEventListener('click', function(event) {
+        if (event.target == deslogarModal) {
+            deslogarModal.style.display = 'none';        
+        }
+    });
+    }); 
