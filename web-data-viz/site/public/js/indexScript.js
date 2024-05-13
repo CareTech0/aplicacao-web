c9e73f5b49e297ea53e133a8f7ec316061e05409
Henrique-Crispino <henrique.crispino@sptech.school>
2024-05-13 12:00:16 -0300

    Mudança no header, manutenção do header da página sobre nós

--- web-data-viz/site/public/js/indexScript.js
+++ web-data-viz/site/public/js/indexScript.js
@@ -6,24 +6,6 @@ $(document).ready(function () {
     pauseOnClick: false,
     speed: 1000,
   });
-
-  const coresPreSelecionadas = ['#A6C0E4', '#3F8EBF', '#90BEDA'];
-  
-  function changeHeaderColor(index) {
-    let element = document.querySelector('.header');
-    let cor = coresPreSelecionadas[index];
-    element.style.backgroundColor = cor;
-  }
-
-  $(".carousel").on('afterChange', function(event, slick, currentSlide) {
-    changeHeaderColor(currentSlide % coresPreSelecionadas.length);
-  });
-
-  $(".carousel").on('afterChange', function(event, slick, currentSlide) {
-    changeHeaderColor(currentSlide % coresPreSelecionadas.length);
-  });
-
-  changeHeaderColor(0);
 });
 
 const hamburger = document.querySelector(".hamburger");
