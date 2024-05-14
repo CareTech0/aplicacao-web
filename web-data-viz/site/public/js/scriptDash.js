f8bbd831de6cbf4af06a91f59d1b78d2ed876f7a
amandageovanna <amanda.sousa@sptech.school>
2024-05-14 14:44:25 -0300

    adicionando modal de gerenciar maquinas

--- web-data-viz/site/public/js/scriptDash.js
+++ web-data-viz/site/public/js/scriptDash.js
@@ -1,22 +1,3 @@
-function direcionarRAM() {
-  window.location.href = "dashboardFilhaRAM.html";
-}
-function direcionarDISCO() {
-  window.location.href = "dashboardFilhaDISCO.html";
-}
-
-function direcionarREDE() {
-  window.location.href = "dashboardFilhaREDE.html";
-}
-
-function direcionarCPU() {
-  window.location.href = "dashboardFilhaCPU.html";
-}
-
-function voltar() {
-  window.location.href = "dashboard.html";
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   function abrirFiltro() {
     var filtro = document.getElementById("filtro");
@@ -31,5 +12,45 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   document.getElementById("botaoFiltro").addEventListener("click", abrirFiltro);
-  
+});
+
+function abrirModal() {
+  const gerenciarModal = document.getElementById("gerenciarModal");
+
+  function abrirGerenciarModal() {
+    gerenciarModal.style.display = "block";
+    console.log("Modal aberto");
+  }
+
+  const gerenciarBotao = document.querySelectorAll(".fundo-botao");
+  const fecharBotao = document.querySelectorAll(".closeDeslogar");
+
+  gerenciarBotao.forEach(function (button) {
+    button.addEventListener("click", function () {
+      abrirGerenciarModal();
+    });
+  });
+
+  fecharBotao.forEach(function (button) {
+    button.addEventListener("click", function () {
+      gerenciarModal.style.display = "none";
+    });
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == gerenciarModal) {
+      gerenciarModal.style.display = "none";
+    }
+    var filtro = document.getElementById("filtro");
+    if (
+      !event.target.closest(".box-botao-filtro") &&
+      filtro.style.display === "block"
+    ) {
+      filtro.style.display = "none";
+      console.log("filtro fechadoooooooooooooooo");
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  abrirModal();
 });
