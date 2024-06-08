b0b23fce22036a8e985c9595a3eb0f255f97a2af
Thawan Bruno Belo Rodrigues <thawan.rodriues@sptech.school>
2024-06-08 12:44:08 -0300

    Refinando dados do alerta para para o front

--- web-data-viz/site/public/js/session.js
+++ web-data-viz/site/public/js/session.js
@@ -27,12 +27,19 @@ function buscarDadosAlerta(dadosDasMaquinas){
             if(resposta.ok){
                 resposta.json().then(function (resposta){
                     resposta.forEach(dados => {
+                        const tabelaCriticosHoje = document.getElementById('comp_estado_critico_hoje_table');
                         const capacidadeTotal = dados.capacidade_total;
                         const usoCapacidade = dados.uso_capacidade;
                         const nomeHardware = dados.nome_hardware;
 
+                        let usoPorcentagem = 0;
+                        if(nomeHardware != 'rede'){
+                            if(nomeHardware == 'disco' || nomeHardware == 'ram') usoPorcentagem = (usoCapacidade * 100) / capacidadeTotal;
+                            else usoPorcentagem = usoCapacidade;
+                        }
+
                         console.log(`
-                            ${capacidadeTotal} ${usoCapacidade} ${nomeHardware}
+                            ${capacidadeTotal} ${usoCapacidade} ${nomeHardware} ${usoPorcentagem}
                         `);
 
                     });  
